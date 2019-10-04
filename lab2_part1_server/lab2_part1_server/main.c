6ea71e0f577c69d99c38a3a986a8ecd3f44220ec
clega001 <clega001@ucr.edu>
2019-10-03 20:37:20 -0700

    lab2

---
+++ lab2_part1_server/lab2_part1_server/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab2_part1_server.c
+ *
+ * Created: 10/3/2019 8:34:50 PM
+ * Author : Christian Legaspino
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
