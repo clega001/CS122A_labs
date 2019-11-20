dfe9a96af37d7c13f7d4140fe598677325f6de7a
clega001 <clega001@ucr.edu>
2019-11-19 16:07:43 -0800

    update

--- one/one/main.c
+++ one/one/main.c
@@ -10,8 +10,7 @@
 #include <util/delay.h>
 #include "pwm.c"
 
-unsigned short t = 0x0000;
-unsigned short k = 0x0000;
+unsigned short a, b, c, d, e, f, g, h;
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
@@ -21,126 +20,133 @@ void ADC_init()
 	ADMUX=(1<<REFS1)|(1<<REFS0);                         // For Aref=AVcc;
 	ADCSRA=(1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //Rrescalar div factor =128
 }
-uint16_t ReadADC(uint8_t ch)
-{
+uint16_t ReadADC(uint8_t ch){//I added the small delays
 	//Select ADC Channel ch must be 0-7
 	ch=ch&0b00000111;
-	
 	ADMUX|=ch;
-	
-	// I added here: allows time to switch between analog channels
 	_delay_ms(1);
-
+	
 	//Start Single conversion
 	ADCSRA|=(1<<ADSC);
-	
 	_delay_ms(1);
-
+	
 	//Wait for conversion to complete
 	while(!(ADCSRA & (1<<ADIF)));
-
 	//Clear ADIF by writing one to it
 	//Note you may be wondering why we have write one to clear it
 	//This is standard way of clearing bits in io as said in datasheets.
 	//The code writes '1' but it result in setting bit to '0' !!!
-
 	ADCSRA|=(1<<ADIF);
-
 	return(ADC);
 }
 
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
-// Implement scheduler code from PES.
 int main(){
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
 
- 	//Init ADC for Interrupt
-// 	ADCSRA |= 1<<ADPS2; //Set ADC Pre-scaler
-// 	ADMUX |= 1<<REFS0 | 1<<REFS1; // Voltage Reference 
-// 	ADCSRA |= 1<<ADIE; //ADC Interrupt Flag
-// 	ADCSRA |= 1<<ADEN; //ADC Enable
-// 
-// 	sei();
-// 	
-// 	ADCSRA |= (1<<ADSC); //Start ADC Conversion 
-	
-	
 	ADC_init();
-	ADMUX = 0xC1;
-	ADCSRA |= (1<<ADSC); //Start ADC Conversion
-	_delay_ms(5); 
+// 	ADMUX = 0xC0;
+// 	ADCSRA |= (1<<ADSC); //Start ADC Conversion
+// 	_delay_ms(5); 
 	
-
 	while(1) {
-		t = ReadADC(1);
-		//t = ADC;
-			if(t > 240){
-				PORTB = 0x01;
-			}
-			else if(t > 100){
-				PORTB = 0x02;
-			}
-			else{
-				PORTB = 0x04;
-			}	
-			
-		_delay_ms(10);
-		ADMUX = 0xC2;
+		ADMUX = 0xC0;
 		ADCSRA |= (1<<ADSC);
 		_delay_ms(5);
 		
-		k = ReadADC(2);
-		//k = ADC;
-			if(k > 240){
-				PORTD = 0x01;
-			}
-			else if(k > 100){
-				PORTD = 0x02;
+		a = ReadADC(0);
+			if(a > 150){
+				PORTC = 0x00;
 			}
 			else{
-				PORTD = 0x04;
-			}
-
-		_delay_ms(10);
+				PORTC = 0xE0;
+			}	
+			
 		ADMUX = 0xC1;
 		ADCSRA |= (1<<ADSC);
 		_delay_ms(5);
 		
+		b = ReadADC(1);
+			if(b > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x70;
+			}
+
+		ADMUX = 0xC2;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
+		c = ReadADC(2);
+			if(c > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x38;
+			}
+
+		ADMUX = 0xC3;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
+		d = ReadADC(3);
+			if(d > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x1C;
+			}
+
+		ADMUX = 0xC4;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
+		e = ReadADC(4);
+			if(e > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x0E;
+			}
+			
+		ADMUX = 0xC5;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
+		f = ReadADC(5);
+			if(f > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x07;
+			}
+			
+		ADMUX = 0xC6;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
+		g = ReadADC(6);
+			if(g > 150){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = 0x03;
+			}
+			
+		ADMUX = 0xC7;
+		ADCSRA |= (1<<ADSC);
+		_delay_ms(5);
 		
-		
-// 		t = ReadADC(1);
-// 		if(t > 240){
-// 			PORTB = 0x01;
-// 		}
-// 		else if(t > 100){
-// 			PORTB = 0x02;
-// 		}
-// 		else{
-// 			PORTB = 0x04;
-// 		}
-//		_delay_ms(10);
-// 		k = ReadADC(2);
-// 		if(k > 240){
-// 			PORTD = 0x01;
-// 		}
-// 		else if(k > 100){
-// 			PORTD = 0x02;
-// 		}
-// 		else{
-// 			PORTD = 0x04;
-// 		}
-		
-		
-		
-		
+		h = ReadADC(7);
+		if(h > 150){
+			PORTC = 0x00;
+		}
+		else{
+			PORTC = 0x01;
+		}
 	}
 	return 0;
 }
---
+++ two/two/main.c
@@ -0,0 +1,18 @@
+/*
+ * two.c
+ *
+ * Created: 11/18/2019 9:28:24 PM
+ * Author : Christian Legaspino
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
